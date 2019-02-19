13a348b467ef95b389459b205eb10bcc02d369e9
Chungsang Tom Lam <erinata@gmail.com>
2019-02-19 08:23:36 -0500

    get movie count

--- tmdb_request.py
+++ tmdb_request.py
@@ -1,12 +1,23 @@
 import urllib.request
 import sys
+import os
+import json
 
 api_key = sys.argv[1]
 
-response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/42728?api_key=' + api_key)
 
-print(response.read())
+if not os.path.exists("json_files"):
+	os.mkdir("json_files")
 
+response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
 
+json_response = json.load(response)
 
-# 
+movie_count = int(json_response['id'])
+
+print(movie_count)
+
+# response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/42728?api_key=' + api_key)
+
+# print(response.read())
+

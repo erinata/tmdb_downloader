55a77a9d54d9df062bf426238092b78a640a1b71
Chungsang Tom Lam <erinata@gmail.com>
2019-02-19 08:24:26 -0500

    gitignore

--- tmdb_request.py
+++ tmdb_request.py
@@ -9,13 +9,16 @@ api_key = sys.argv[1]
 if not os.path.exists("json_files"):
 	os.mkdir("json_files")
 
+if not os.path.exists("parsed_files"):
+	os.mkdir("parsed_files")
+
 response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
 
 json_response = json.load(response)
 
 movie_count = int(json_response['id'])
 
-print(movie_count)
+
 
 # response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/42728?api_key=' + api_key)
 

09d1510f274fb7eee365ced1333e293cfc828baa
Chungsang Tom Lam <erinata@gmail.com>
2019-02-19 08:36:21 -0500

    write to json

--- tmdb_request.py
+++ tmdb_request.py
@@ -17,14 +17,17 @@ response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api
 json_response = json.load(response)
 movie_count = int(json_response['id'])
 
-movie_start = movie_count-10
+movie_start = movie_count-5
 
 for i in range(movie_start, movie_count):
 	print("downloading: " + str(i))
 	response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/' + str(i) + '?api_key=' + api_key)
-	print(response.read())
-	time.sleep(10)
-
+	# print(response.read())
+	json_response = json.load(response)
+	f = open("json_files/tmdb" + str(i) + ".json", "w")
+	f.write(str(json_response))
+	f.close()
+	time.sleep(15)
 
 
 # print(response.read())
